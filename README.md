e09f3c6b78f47571ee1b3b3f2e2d5e0e6192cc3d
zhusong <song.zhu@lasong.com.cn>
2020-07-29 14:18:34 +0800

    更新base库

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 * 设备分辨率转换类 **DeviceUtils**
 * Activity&Application基类
 * 应用管理类 **记录** Activity生命周期
+
+## v0.0.2
+* 添加文件Uri解析绝对路径工具
+* BaseActivity添加权限申请方法
--- src/main/java/cn/com/lasong/base/BaseActivity.java
+++ src/main/java/cn/com/lasong/base/BaseActivity.java
@@ -1,6 +1,10 @@
 package cn.com.lasong.base;
 
+import android.content.pm.PackageManager;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 /**
  * Author: zhusong
@@ -10,5 +14,17 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class BaseActivity extends AppCompatActivity {
 
+    protected final int REQ_PERMISSION = 0x007;
 
+    protected boolean requestAllPermissions(String... permissions) {
+        int length = permissions.length;
+        for (int i = 0; i < length; i++) {
+            if(ContextCompat.checkSelfPermission(this, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, permissions,
+                        REQ_PERMISSION);
+                return false;
+            }
+        }
+        return true;
+    }
 }
